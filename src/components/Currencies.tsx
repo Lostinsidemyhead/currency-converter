e2db25ed4ddcec5767f30ba5d1f2a331928189bc
Ilia Anferov <anferov.ilia@gmail.com>
2021-11-08 23:49:06 +0700

    currencies logic, navbar

--- src/components/Currencies.tsx
+++ src/components/Currencies.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchRate } from '../api/CurrencyService';
+
+enum currencies {
+  rub = 'RUB',
+  usd = 'USD',
+  eur = 'EUR'
+}
 
 const Currencies: React.FC = () => {
+  const [currency, setCurrency] = useState<currencies>(currencies.usd);
+  const [rates, setRates] = useState<number[]>();
+
+  useEffect(() => {
+    // const userLang = navigator.language;
+    // switch (userLang) {
+    //   case 'ru-RU':
+    //     setCurrency(currencies.rub);
+    //     break;
+    //   case 'en':
+    //     setCurrency(currencies.usd);
+    //     break;
+    //   default:
+    //     setCurrency(currencies.eur);
+    // }
+    fillTable();
+  }, []);
+
+  useEffect(() => {
+
+  }, [currency]);
+
+  const fillTable = async () => {
+    const rates = await fetchRate();
+    const currentRate = rates[currency]?.Value || 1;
+
+    const RUBRate = 1 / currentRate;
+    const USDRate = rates.USD.Value / currentRate;
+    const EURRate = rates.EUR.Value / currentRate;
+
+    setRates([RUBRate, USDRate, EURRate]);
+
+  }
+
   return (
     <div>
+      {rates?.map((rate) => (
 
+        <div>
+          {rate}
+        </div>
+
+      ))}
     </div>
   )
 }
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
   return (
-    <div>
-      
+    <div className="navbar__links">
+      <Link to="/">Converter</Link>
+      <Link to="/table">Currencies</Link>
     </div>
   )
 }
